b1109778a4e47e93b69d826cc2ca7ce5ff131293
SUNIL PANDEY <97494052+sunil9454@users.noreply.github.com>
2022-04-08 20:05:46 +0530

    Add files via upload

---
+++ wwwroot/js/Custom.js
@@ -0,0 +1,30 @@
+﻿
+$(function () {
+    $("body").on('mousemove', function () {
+        ResetThisSession();
+    });
+
+});
+var timeInSecondsAfterSessionOut = 300;
+var secondTick = 0;
+function ResetThisSession() {
+    secondTick = 0;
+}
+function StartThisSessionTimer() {
+    secondTick++;
+    var timeLeft = ((timeInSecondsAfterSessionOut - secondTick) / 60).toFixed(0);
+    timeLeft = timeInSecondsAfterSessionOut - secondTick;
+    if (timeLeft == 60) {
+        $('#Warning_TimeLeft').modal('show');
+    }
+    $("#spanTimeLeft").html(timeLeft);
+
+    if (secondTick > timeInSecondsAfterSessionOut) {
+        clearTimeout(tick);
+        window.location = "/Account/SignOut";
+        return;
+    }
+    tick = setTimeout("StartThisSessionTimer()", 1000);
+}
+StartThisSessionTimer();
+
---
+++ wwwroot/js/Loader.js
@@ -0,0 +1,18 @@
+﻿$(document).on('click', '#btnSave', function (e) {
+    e.preventDefault();
+    var spinner = $('#loader');
+    var obj = {
+        name: $('#Name').val()
+    };
+    spinner.show();
+    $.ajax({
+        type: 'POST',
+        url: '/Loader/Loader',
+        dataType: 'json',
+        data: obj,
+    }).done(function (response) {
+        spinner.hide();
+        alert(response.name);
+    });
+
+});

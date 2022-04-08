c52dd2f24ad03ea59509579b3b957f724c76989d
SUNIL PANDEY <97494052+sunil9454@users.noreply.github.com>
2022-04-08 20:06:58 +0530

    Add files via upload

---
+++ Controllers/LoaderController.cs
@@ -0,0 +1,40 @@
+﻿using EmployeeCrudOperation.Models;
+using EmployeeCrudOperation.Models.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeCrudOperation.Controllers
+{
+    public class LoaderController : Controller
+    {
+        private ApplicationContext context;
+       
+
+        public LoaderController(ApplicationContext _context)
+        {
+            context = _context;
+        }
+        public IActionResult Loader()
+        {
+            return View();
+        }
+        [HttpPost]
+        public JsonResult Loader(Loader model)
+        {
+            var data = new Loader()
+            {
+                Name = model.Name
+            };
+            context.Loaders.Add(data);
+            context.SaveChanges();
+            return new JsonResult(data);
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

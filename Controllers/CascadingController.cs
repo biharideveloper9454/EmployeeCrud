56b752cc1c02c96f5b947f8b07576ec2cd485639
SUNIL PANDEY <97494052+sunil9454@users.noreply.github.com>
2022-04-11 18:10:09 +0530

    Add files via upload

---
+++ Controllers/CascadingController.cs
@@ -0,0 +1,41 @@
+﻿using EmployeeCrudOperation.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeCrudOperation.Controllers
+{
+    public class CascadingController : Controller
+    {
+        private readonly ApplicationContext context;
+
+        public CascadingController(ApplicationContext context)
+        {
+            this.context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult GetCountries()
+        {
+            var country = context.Countries.ToList();
+            return new JsonResult(country);
+        }
+
+        public JsonResult GetStates(int id)
+        {
+            var state = context.States.Where(e => e.Country.Id == id).ToList();
+            return new JsonResult(state);
+        }
+        public JsonResult GetCities(int id)
+        {
+            var city = context.Cities.Where(e => e.State.Id == id).ToList();
+            return new JsonResult(city);
+        }
+    }
+}
